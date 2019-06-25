d3a8f7eb68751c115f7a717b1b8bcb76ba55c67c
lali2 <lali2@cisco.com>
2019-06-25 21:22:43 +0800

    stable version can run in server

--- ph3/kdd_train.py
+++ ph3/kdd_train.py
@@ -8,42 +8,41 @@ def train():
 @timed()
 def train_base(feature_cnt=9999):
 
-    all_data = get_feature_all()
+    all_data = get_feature_all().sample(frac=0.5)
     # Define F1 Train
-
-    # CV TRAIN
-    from collections import Counter
-
     feature_name = get_feature_name(all_data)[:feature_cnt]
+    logger.debug(f'Final Train feature#{len(feature_name)}: {list(feature_name)}')
+    # CV TRAIN
+
     tr_index = ~all_data['click_mode'].isnull()
     X_train = all_data[tr_index][list(set(feature_name))].reset_index(drop=True)
     y = all_data[tr_index]['click_mode'].reset_index(drop=True)
     X_test = all_data[~tr_index][list(set(feature_name))].reset_index(drop=True)
-    print(X_train.shape,X_test.shape)
+    print(X_train.shape, X_test.shape)
     final_pred = []
     cv_score = []
     cv_model = []
     skf = StratifiedKFold(n_splits=5, random_state=2019, shuffle=True)
     for index, (train_index, test_index) in enumerate(skf.split(X_train, y)):
-        with timed_bolck(f'CV_Folder#{index}'):
-            lgb_model = lgb.LGBMClassifier(
-                boosting_type="gbdt", num_leaves=128, reg_alpha=0.1, reg_lambda=10,
-                max_depth=-1, n_estimators=3000, objective='multiclass',num_classes=12,
-                subsample=0.5, colsample_bytree=0.5, subsample_freq=1,
-                learning_rate=0.1, random_state=2019 + index, n_jobs=40, metric="None", importance_type='gain'
-            )
-            train_x, test_x, train_y, test_y = X_train[feature_name].iloc[train_index], X_train[feature_name].iloc[test_index], y.iloc[train_index], y.iloc[test_index]
-            eval_set = [(test_x[feature_name], test_y)]
-            lgb_model.fit(train_x[feature_name], train_y, eval_set=eval_set,verbose=10,early_stopping_rounds=30,eval_metric=f1_macro)
-            cv_model.append(lgb_model)
-            y_test = lgb_model.predict(X_test[feature_name])
-            y_val = lgb_model.predict_proba(test_x[feature_name])
-            print(Counter(np.argmax(y_val,axis=1)))
-            cv_score.append(get_f1_score(test_y,y_val))
-            if index == 0:
-                final_pred = np.array(y_test).reshape(-1, 1)
-            else:
-                final_pred = np.hstack((final_pred, np.array(y_test).reshape(-1, 1)))
+        gc.collect()
+        # with timed_bolck(f'Folder#{index}, feature:{len(feature_name)}'):
+        lgb_model = lgb.LGBMClassifier(
+            boosting_type="gbdt", num_leaves=128, reg_alpha=0.1, reg_lambda=10,
+            max_depth=-1, n_estimators=3000, objective='multiclass', num_classes=12,
+            subsample=0.5, colsample_bytree=0.5, subsample_freq=1,
+            learning_rate=0.1, random_state=2019 + index, n_jobs=10, metric="None", importance_type='gain'
+        )
+        train_x, test_x, train_y, test_y = X_train[feature_name].iloc[train_index], X_train[feature_name].iloc[
+            test_index], y.iloc[train_index], y.iloc[test_index]
+        eval_set = [(test_x[feature_name], test_y)]
+        logger.info(f'Begin Train#{index}, feature:{len(feature_name)}, Size:{train_x[feature_name].shape}')
+        lgb_model.fit(train_x[feature_name], train_y, eval_set=eval_set, verbose=10, early_stopping_rounds=30,
+                      eval_metric=f1_macro)
+
+        cv_model.append(lgb_model)
+        y_test = lgb_model.predict(X_test[feature_name])
+        y_val = lgb_model.predict_proba(test_x[feature_name])
+        print(Counter(np.argmax(y_val, axis=1)))
     # fi = []
     # for i in cv_model:
     #     tmp = {
